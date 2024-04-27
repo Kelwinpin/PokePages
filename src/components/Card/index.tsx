197d44005add69b1313db0711fe77dd617ce6715
Kelwinpin <Kelwincomw@gmail.com>
2024-04-27 13:12:32 -0300

    add Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,10 @@
+import { StyledCard } from "./styles";
+
+export function Card ({id, name, sprites}: Pokemon){
+    return(
+        <StyledCard key={id}>
+            <img src={sprites.front_shiny} alt={`${name}-Image`} width="300px"/>
+            <h3>{name}</h3>
+        </StyledCard>
+    )
+}
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledCard = styled.div`
+    display: flex;
+    border: 1px solid green;
+    flex-direction: column;
+    align-items: center;
+    width: fit-content;
+`

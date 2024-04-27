cb348e56740cdeae4b4e117162ea3699d3bb0d3d
Kelwinpin <Kelwincomw@gmail.com>
2024-04-27 13:12:51 -0300

    refactor api

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-export async function searchPokemon<T>(params:string): Promise<T[]>{
-    try {
-        const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${params}/`);
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
-    return [];
+export async function searchPokemon(params:string): Promise<Pokemon>{
+        try {
+            const response: AxiosResponse<any, any> = await axios.get(`https://pokeapi.co/api/v2/pokemon/${params}`);
+            const pokemonData: Pokemon = response.data;
+            return pokemonData;
+        } catch (error) {
+            throw new Error("Error ao obter dados do Pokemon!");
+        }
 }
